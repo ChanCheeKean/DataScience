90691c98a002e8a384dc69edf66dcbfb0f47e338
cheekean <cheekeanchan@CHANs-MacBook-Pro.local>
2023-08-20 11:10:06 +0800

    updated nlp stack

--- 82 - Cloud/emr/03 - step_function/write.py
+++
@@ -1,7 +0,0 @@
-def to_files(df, tgt_dir, file_format):
-    df.coalesce(16). \
-        write. \
-        partitionBy('year', 'month', 'dayofmonth'). \
-        mode('append'). \
-        format(file_format). \
-        save(tgt_dir)
